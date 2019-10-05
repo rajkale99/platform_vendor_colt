BeastRoms/platform_vendor_beast

vendor: google: Add some google apps
* Markup
* SoundPicker
* Wellbeing

Signed-off-by: mdeejay <mdjrussia@gmail.com>

config/common.mk
@@ -27,7 +27,7 @@ endif
 
 # Bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/beast/prebuilt/common/media/bootanimation.zip:system/product/media/bootanimation.zip \
+    vendor/beast/prebuilt/common/media/bootanimation.zip:system/product/media/bootanimation.zip
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
     mkfs.exfat
 endif
 
+# Markup libs
+PRODUCT_COPY_FILES += \
+    vendor/beast/prebuilt/google/lib/libsketchology_native.so:system/product/lib/libsketchology_native.so \
+    vendor/beast/prebuilt/google/lib64/libsketchology_native.so:system/product/lib64/libsketchology_native.so
 
 # We modify several neverallows, so let the build proceed
 ifneq ($(TARGET_BUILD_VARIANT),user)

rajkale99/platform_vendor_colt

Change-Id: I73ddba26c4556244730a0e4f3e80e370fc323401
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

config/common.mk
@@ -97,6 +97,11 @@ PRODUCT_COPY_FILES += \
     vendor/colt/prebuilt/common/etc/init/colt-system.rc:system/etc/init/colt-system.rc \
     vendor/colt/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
+# Markup libs
+PRODUCT_COPY_FILES += \
+    vendor/colt/prebuilt/google/lib/libsketchology_native.so:system/product/lib/libsketchology_native.so \
+    vendor/colt/prebuilt/google/lib64/libsketchology_native.so:system/product/lib64/libsketchology_native.so
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/colt/prebuilt/common/addon.d/50-colt.sh:system/addon.d/50-colt.sh \
@@ -116,7 +121,9 @@ PRODUCT_COPY_FILES += \
 # Colt packages
 PRODUCT_PACKAGES += \
     NexusLauncherRelease \
-    GalleryGoPrebuilt
+    GalleryGoPrebuilt \
+    MarkupGoogle \
+    SoundPickerPrebuilt
 
 # Overlays
 PRODUCT_PACKAGES += \